c771ef54e05018269e38a22dc39d54b0ff847554
Dhanush V <118054875+Dhanush0724@users.noreply.github.com>
2024-07-02 19:46:48 +0530

    Add files via upload

--- oops.py
+++ oops.py
@@ -1,28 +1,39 @@
-class Book:
-    def __init__(self,title,author,isbn) :
-        self.title = title
-        self.author = author
-        self.isbn = isbn
-        self.available = True
+# class student:
+#     def __init__(self,name,age,grade):
+#         self.name = name
+#         self.age = age
+#         self.grade = grade
 
-class Patron:
+#     def print(self):
+#         print("Name :",self.name)
+#         print("Age:",self.age)
+#         print("Grade:",self.grade)
 
-    def __init__(self,name,patron_id):
-        self.name = name
-        self.patron_id = patron_id
-        self.borrowed_books = []
+# if __name__ == "__main__":
 
-    def borrow_book(self,book):
-        if book.available:
-            self.borrowed_books.append(book)
-            book.available = False
-            print(f"{self.name} borrowed {self.book}")
+#     stu = student("james",20,9)
+#     stu.print()
 
+class Rectangel:
 
-class library:
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+    
+    def area(self):
+        return self.length*self.width
 
-    def __init__(self) :
+    def perimeter(self):
+        return 2*(self.length+self.width)
+    
+    def __str__(self) :
+        return f"Rectangle(length={self.length},width = {self.width})"
+    
+if __name__ == "__main__":
 
-        
-        
-        
+    r1 = Rectangel(5,3)
+    print(r1)
+
+    print(f"Area:{r1.area()}")
+    print(f"perimter:{r1.perimeter()}")
+    
